2c49f146cbbb2386405cc2af4ac0ea408332f769
Shaun Hoyes <jshoyes@gmail.com>
2016-02-21 17:46:16 -0500

    Update btc.sh

--- btc.sh
+++ btc.sh
@@ -8,5 +8,5 @@ do
 curl -s http://api.coindesk.com/v1/bpi/currentprice.json |
 python -c "import json, sys; print json.load(sys.stdin)['bpi']['USD']['rate']"
 printf "\033[A" # restarts the script
-sleep 10 # 10 seconds for refresh
+sleep 10 # price will refresh every 10 seconds
 done
