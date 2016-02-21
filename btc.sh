bc8c7796fca9d17d0f41c6f6c97b457a5e26c967
Shaun Hoyes <jshoyes@gmail.com>
2016-02-21 17:45:17 -0500

    Added files via upload

---
+++ btc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Credit to David Walsh - <https://davidwalsh.name/bitcoin>
+
+echo "Coindesk BTC price USD: "
+
+while [ 1 ] # while always
+do
+curl -s http://api.coindesk.com/v1/bpi/currentprice.json |
+python -c "import json, sys; print json.load(sys.stdin)['bpi']['USD']['rate']"
+printf "\033[A" # restarts the script
+sleep 10 # 10 seconds for refresh
+done
