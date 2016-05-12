fa8a850dd14f475fba9dfabd4631725e4fb917e3
Shaun Hoyes <ShaunHoyes@users.noreply.github.com>
2016-05-12 14:21:33 -0400

    rounding off to two decimal places
    This update is to provide only two decimal places for the figures (expressed in U.S. dollars).

--- CoinTable.py
+++ CoinTable.py
@@ -1,43 +1,43 @@
+#!/usr/bin/env python3
+
 from decimal import Decimal
 from prettytable import PrettyTable
+
 import time, json, requests
 
 def btc():
     btcPrice = requests.get('https://www.cryptonator.com/api/ticker/btc-usd')
-    return btcPrice.json()['ticker']['price']
+    return round(float(btcPrice.json()['ticker']['price']), 2)
 
 def eth():
     ethPrice = requests.get('https://www.cryptonator.com/api/ticker/eth-usd')
-    return ethPrice.json()['ticker']['price']
+    return round(float(ethPrice.json()['ticker']['price']), 2)
 
 def ltc():
     ltcPrice = requests.get('https://www.cryptonator.com/api/ticker/ltc-usd')
-    return ltcPrice.json()['ticker']['price']
+    return round(float(ltcPrice.json()['ticker']['price']), 2)
 
 def dash():
     dashPrice = requests.get('https://www.cryptonator.com/api/ticker/dash-usd')
-    return dashPrice.json()['ticker']['price']
+    return round(float(dashPrice.json()['ticker']['price']), 2)
 
 def sjcx():
     sjcxPrice = requests.get('https://www.cryptonator.com/api/ticker/sjcx-usd')
-    return sjcxPrice.json()['ticker']['price']
+    return round(float(sjcxPrice.json()['ticker']['price']), 2)
 
 def xcp():
     xcpPrice = requests.get('https://www.cryptonator.com/api/ticker/xcp-usd')
-    return xcpPrice.json()['ticker']['price']
+    return round(float(xcpPrice.json()['ticker']['price']), 2)
 
-def fct():
-    fctPrice = requests.get('https://www.cryptonator.com/api/ticker/fct-usd')
-    return fctPrice.json()['ticker']['price']
 
 coins = PrettyTable(['Coin', 'Price'])
 
-coins.add_row(['Bitcoin BTC:', "$" + btc() + "\n"])
-coins.add_row(['Ethereum ETH:', "$" + eth() + "\n"])
-coins.add_row(['Litecoin LTC:', "$" + ltc() + "\n"])
-coins.add_row(['Dash DASH:', "$" + dash() + "\n"])
-coins.add_row(['Factom FCT:', "$" + fct() + "\n"])
-coins.add_row(['Counterparty XCP:', "$" + xcp() + "\n"])
-coins.add_row(['Storjcoin SJCX:', "$" + sjcx()])
+coins.add_row(['Bitcoin BTC:', "$" + str(btc()) + "\n"])
+coins.add_row(['Ethereum ETH:', "$" + str(eth()) + "\n"])
+coins.add_row(['Litecoin LTC:', "$" + str(ltc()) + "\n"])
+coins.add_row(['Dash DASH:', "$" + str(dash()) + "\n"])
+coins.add_row(['Counterparty XCP:', "$" + str(xcp()) + "\n"])
+coins.add_row(['Storjcoin SJCX:', "$" + str(sjcx()) + "\n"])
 
-print coins;
+
+print(coins);
